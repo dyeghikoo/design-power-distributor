973641652aa2783f44b723777770d6431be867b1
Diego Sierra Fernandez <diegosierr28@hotmail.com>
2017-12-17 20:45:23 +0100

    Add files via upload

---
+++ Step 1/MyWorkspace_wrk/workspace.ads
@@ -0,0 +1,35 @@
+<ADSWorkspace Revision="2" Version="100">
+    <Workspace Name="">
+        <Library Name="ads_standard_layers" />
+        <Library Name="adstechlib" />
+        <Library Name="ads_schematic_layers" />
+        <Library Name="empro_standard_layers" />
+        <Library Name="ads_standard_layers_ic" />
+        <Library Name="ads_schematic_layers_ic" />
+        <Library Name="ads_schematic_ports_ic" />
+        <Library Name="ads_rflib" />
+        <Library Name="ads_sources" />
+        <Library Name="ads_simulation" />
+        <Library Name="ads_tlines" />
+        <Library Name="ads_bondwires" />
+        <Library Name="ads_datacmps" />
+        <Library Name="ads_behavioral" />
+        <Library Name="ads_textfonts" />
+        <Library Name="ads_common_cmps" />
+        <Library Name="ads_designs" />
+        <Library Name="ads_verification_test_bench" />
+        <Library Name="MyLibrary_lib" />
+        <Cell Name="MyLibrary_lib:cell_1" />
+        <Data_Display Name="cell_1.dds" />
+        <Dataset Name="cell_1.ds" />
+        <Log Name="netlist.log" />
+        <Log Name="search_history.log" />
+        <Preferences Name="layout.prf" />
+        <Preferences Name="MyLibrary_lib_lay.prf" />
+        <Preferences Name="schematic.prf" />
+        <LibraryDefs Name="lib.defs" />
+        <ConfigFile Name="dds.cfg" />
+        <ConfigFile Name="de_sim.cfg" />
+        <ConfigFile Name="hpeesofsim.cfg" />
+    </Workspace>
+</ADSWorkspace>
